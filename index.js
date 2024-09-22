600c49a5f0c4f4fde029e1136a283b37a8ba96ed
hamim <hamim2114@gmail.com>
2024-09-22 07:25:35 +0600

    completed

--- index.js
+++ index.js
@@ -49,8 +49,9 @@ connect();
 
 app.use(cors({origin: [
   'http://localhost:4000',
-  'http://localhost:5173'
-  // 'https://ahmedadmin.vercel.app'
+  'http://localhost:5173',
+  'https://ahmedadmin.vercel.app',
+  'https://ahmedassociates.vercel.app'
 ], credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
